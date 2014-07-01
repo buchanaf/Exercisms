75edffd51eebcbeb563a03e5dcefcf15c6f2d9a5
buchanaf <buchanaf@gmail.com>
2014-07-01 11:20:05 -0400

    compelted pythaTest.js

---
+++ pythagorean-triplet/pythagTest.js
@@ -0,0 +1,76 @@
+var Triplet = function(one, two, three){
+
+  var trips = []
+  var args = arguments;
+
+  for(var i = 0; i<args.length;i++){
+    trips.push(args[i])
+  }
+
+  this.sum = function(){
+    var result = 0;
+    trips.forEach(function(item){
+      result += item;
+    })
+
+    return result
+  }
+
+  this.product = function(){
+    var result = 1;
+    trips.forEach(function(item){
+      result = result * item;
+    })
+
+    return result
+  }
+
+  this.isPythagorean = function(){
+    if(Math.pow(one, 2) + Math.pow(two, 2) === Math.pow(three, 2)){
+      return true
+    } else {
+      false
+    }
+  }
+
+}
+
+
+
+Triplet.where = function(minMax){
+  var minMax = minMax
+  var results = [];
+
+  for (var i = minMax.minFactor || 1; i <= minMax.maxFactor; i++){
+    for (var j = minMax.minFactor || 1; j <= minMax.maxFactor; j++){
+      for (var k = minMax.minFactor || 1; k <= minMax.maxFactor; k++){
+        if(new Triplet(i,j,k).isPythagorean()){
+          if(minMax.sum){
+            if((i + j + k) === minMax.sum){
+              results.push(new Triplet(i,j,k));
+            }
+          } else {
+            results.push(new Triplet(i,j,k));
+          }
+        }
+      }
+    }
+  }
+
+  var counterObject = {};
+  var finalResults = []
+  results.forEach(function(item, index){
+    console.log(item.product())
+    if (counterObject.hasOwnProperty(item.product())){
+      return
+    } else {
+      counterObject[item.product()] = 1;
+      finalResults.push(item)
+    }
+  })
+
+
+  return finalResults
+
+}
+
